5b95de984e12355a62af667910656d5bc7451064
Tobias Allweier <toallwei@microsoft.com>
2025-01-30 16:57:57 +0100

    sample

---
+++ src/RadzenPlayground/RadzenPlayground.Client/MyTreeItem.cs
@@ -0,0 +1,13 @@
+﻿namespace radzenplayground.Client
+{
+    public class MyTreeItem
+    {
+        public string Text { get; set; }
+
+        public List<MyTreeItem> Items { get; set; } = new List<MyTreeItem>();
+
+        public bool Expanded { get; set; }
+
+        public bool HasChildren => Items.Any();
+    }
+}
---
+++ src/RadzenPlayground/RadzenPlayground/Program.cs
@@ -0,0 +1,37 @@
+using Radzen;
+using RadzenPlayground.Client.Pages;
+using RadzenPlayground.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddRadzenComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(RadzenPlayground.Client._Imports).Assembly);
+
+app.Run();
